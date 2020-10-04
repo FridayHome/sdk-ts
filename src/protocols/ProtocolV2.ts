f733801594f428e60b0cd3cacec9e2a52dae153f
Oliver Fleckenstein <oliverfl@live.dk>
2020-10-04 11:42:31 +0800

    Added Protocol V2

--- src/protocols/ProtocolV2.ts
+++ src/protocols/ProtocolV2.ts
@@ -1,10 +1,32 @@
+import { BitConverter } from '../utils/BitConverter';
 import { Bytes } from '../utils/byteUtils';
 import { IProtocol, ProtocolVersion } from './IProtocol';
 
+const ChallengeLength = 6;
+
+/**
+ * Represents a protocol that can be used to send messages
+ * to the Friday Lock.
+ *
+ * Consists of 8 bytes:
+ * - 2 bytes sequence number
+ * - 6 bytes challenge
+ */
 export class ProtocolV2 implements IProtocol {
-  Version: ProtocolVersion = 2;
+	Version: ProtocolVersion = 2;
+
+	sequenceNumber: number;
+	challenge: Bytes;
+
+	constructor(sequenceNumber = 1, challenge: Bytes = [0, 0, 0, 0, 0, 0]) {
+		if (challenge.length !== ChallengeLength) {
+			throw new Error(`Challenge must be of length ${ChallengeLength}`);
+		}
+		this.sequenceNumber = sequenceNumber;
+		this.challenge = challenge;
+	}
 
 	toBytes(): Bytes {
-		throw new Error('Method not implemented.');
+		return BitConverter.getBytes(this.sequenceNumber, 2).concat(this.challenge);
 	}
 }
--- test/protocols.test.ts
+++ test/protocols.test.ts
@@ -1,3 +1,4 @@
+import { ProtocolV2 } from '../src';
 import { ProtocolV1 } from '../src/protocols/ProtocolV1';
 import { Bytes } from '../src/utils/byteUtils';
 import { fromLockUnoTime } from '../src/utils/timing';
@@ -14,6 +15,31 @@ describe('Protocol V1', () => {
 		[new ProtocolV1(), [0, 0, 0, 0, 1, 0, 0, 0]],
 		[new ProtocolV1(5, fromLockUnoTime(7)), [7, 0, 0, 0, 5, 0, 0, 0]],
 	])('toBytes', (protocol: ProtocolV1, bytes: Bytes) => {
+		expect(protocol.toBytes()).toEqual(bytes);
+	});
+});
+
+describe('Protocol V2', () => {
+	test('equality', () => {
+		const a = new ProtocolV2(4, [1, 2, 3, 4, 5, 6]);
+		const b = new ProtocolV2(4, [1, 2, 3, 4, 5, 6]);
+
+		expect(a).toEqual(b);
+	});
+
+	test.each([[[]], [[1, 2, 3, 4, 5]], [[1234567]]])(
+		'challenge must be of length 6',
+		(challenge: Bytes) => {
+			expect(() => new ProtocolV2(0, challenge)).toThrowError(
+				'Challenge must be of length 6'
+			);
+		}
+	);
+
+	test.each([
+		[new ProtocolV2(5, [1, 2, 3, 4, 5, 6]), [5, 0, 1, 2, 3, 4, 5, 6]],
+		[new ProtocolV2(256, [83, 42, 13, 193, 121, 245]), [0, 1, 83, 42, 13, 193, 121, 245]],
+	])('toBytes', (protocol: ProtocolV2, bytes: Bytes) => {
 		expect(protocol.toBytes()).toEqual(bytes);
 	});
 });
