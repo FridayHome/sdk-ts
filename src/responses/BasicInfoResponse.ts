7a600dd45672efb44aa303fdc5cd29eb715f131a
Oliver Fleckenstein <oliverfl@live.dk>
2020-10-10 13:24:51 +0800

    Parsing basic info response

--- src/responses/BasicInfoResponse.ts
+++ src/responses/BasicInfoResponse.ts
@@ -1,8 +1,73 @@
+import { MessageType } from '../enums/MessageType';
 import { Message } from '../Message';
+import { IProtocol } from '../protocols/IProtocol';
+import { ProtocolV1 } from '../protocols/ProtocolV1';
+import { BitConverter } from '../utils/BitConverter';
 import { Bytes } from '../utils/byteUtils';
 
 export class BasicInfoResponse extends Message {
+	softdevice: number;
+	application: number;
+	bootloader: number;
+	wifi: number;
+	hardware: number;
+	publicKey: Bytes;
+	battery: number;
+
+	constructor(
+		protocol: IProtocol,
+		softdevice: number,
+		application: number,
+		bootloader: number,
+		wifi: number,
+		hardware: number,
+		battery: number,
+		publicKey: Bytes
+	) {
+		super(MessageType.BasicInfoResponse, protocol);
+		this.softdevice = softdevice;
+		this.application = application;
+		this.bootloader = bootloader;
+		this.wifi = wifi;
+		this.hardware = hardware;
+		this.battery = battery;
+		this.publicKey = publicKey;
+	}
+
 	protected createBody(): Bytes {
 		throw new Error('Method not implemented.');
+	}
+
+	public static parse(protocol: IProtocol, bytes: Bytes): BasicInfoResponse {
+		let index = 0;
+		const publicKey = bytes.slice(0, 32);
+		index += 32;
+
+		const softdevice = BitConverter.toInt32(bytes, index);
+		index += 4;
+		const application = BitConverter.toInt32(bytes, index);
+		index += 4;
+		const bootloader = BitConverter.toInt32(bytes, index);
+		index += 4;
+		const hardware = BitConverter.toInt32(bytes, index);
+		index += 4;
+
+		index += 16; // Ignore empty UUID
+		index += 4; // Ignore empty time
+		const battery = BitConverter.toInt32(bytes, index);
+		index += 4;
+		const wifi = BitConverter.toInt32(bytes, index);
+		index += 4;
+
+		return new BasicInfoResponse(
+			protocol,
+			softdevice,
+			application,
+			bootloader,
+			wifi,
+			hardware,
+			battery,
+			publicKey
+		);
 	}
 }
