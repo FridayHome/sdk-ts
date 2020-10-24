91706c646e84a9b7e381b1c052e42931d4642fc3
Oliver Fleckenstein <oliverfl@live.dk>
2020-10-24 10:09:10 +0800

    Added some more encryption tests and documentation

--- src/encryption.ts
+++ src/encryption.ts
@@ -1,20 +1,47 @@
 import sodium from 'libsodium-wrappers';
 
 export class Encryption {
+  /**
+   * Encrypt and sign a byte array with a given
+   * public and private key.
+   *
+   * @param message to be encrypted
+   * @param privateKey of the sender of the message
+   * @param publicKey of the intended receiver of the message
+   * @param nonce Specify a nonce this message should be
+   * encrypted with. NOTE: you should never need to specify
+   * this in production, as a new nonce should be generated
+   * for each message.
+   */
 	public static encrypt(
 		message: Uint8Array,
 		privateKey: Uint8Array,
-		publicKey: Uint8Array
+		publicKey: Uint8Array,
+		nonce: Uint8Array | undefined = undefined
 	): Uint8Array {
-    const nonce = sodium.randombytes_buf(sodium.crypto_box_NONCEBYTES);
-		const encrypted = sodium.crypto_box_easy(message, nonce, publicKey, privateKey);
-    const output = new Uint8Array(nonce.length + encrypted.length);
-    output.set(nonce, 0);
-    output.set(encrypted, nonce.length);
+		if (!nonce) {
+			nonce = sodium.randombytes_buf(sodium.crypto_box_NONCEBYTES);
+    }
+		const encrypted = sodium.crypto_box_easy(
+			message,
+			nonce,
+			publicKey,
+			privateKey
+		);
+		const output = new Uint8Array(nonce.length + encrypted.length);
+		output.set(nonce, 0);
+		output.set(encrypted, nonce.length);
 
-    return output;
+		return output;
 	}
 
+  /**
+   * Descrypt a message
+   *
+   * @param message to be decrypted
+   * @param privateKey of the receiver
+   * @param publicKey of the sender
+   */
 	public static decrypt(
 		message: Uint8Array,
 		privateKey: Uint8Array,
--- test/encryption.test.ts
+++ test/encryption.test.ts
@@ -3,7 +3,7 @@ import { Encryption } from '../src/encryption';
 
 describe('Encryption', () => {
 	test('message', async () => {
-    await sodium.ready;
+		await sodium.ready;
 		const privateKey = sodium.crypto_box_keypair().privateKey;
 		const recipientPublicKey = Uint8Array.from([
 			151,
@@ -53,5 +53,64 @@ describe('Encryption', () => {
 		);
 
 		expect(decrypted).toEqual(message);
+	});
+
+	test('decrypt challenge message', () => {
+		const privateKey = sodium.from_hex(
+			'93cb0d720fdb4a1e202a3b9737c30b409eabd6c9156d488b01d94b833c3942a2'
+		);
+		const publicKey = sodium.from_hex(
+			'4a15a6a548422383f38a3ecfee5ec830877b9bed5af13eaa2c9bf56ca5221976'
+		);
+		const encrypted = sodium.from_hex(
+			'78326e930590fee7b52d7d962885b4bc42a788fbdaf63f89d4aadc24d4eb85288c7cbe7151c0f63f61d39fb000003c17fb262b84'
+		);
+
+		const decrypted = Encryption.decrypt(encrypted, privateKey, publicKey);
+
+		const expected = sodium.from_hex('000202000400d321f80deb86');
+
+		expect(decrypted).toEqual(expected);
+	});
+
+	test('encrypt challenge message', () => {
+		const privateKey = sodium.from_hex(
+			'93cb0d720fdb4a1e202a3b9737c30b409eabd6c9156d488b01d94b833c3942a2'
+		);
+		const publicKey = sodium.from_hex(
+			'4a15a6a548422383f38a3ecfee5ec830877b9bed5af13eaa2c9bf56ca5221976'
+		);
+		const message = sodium.from_hex('000202000400d321f80deb86');
+		const nonce = sodium.from_hex(
+			'78326e930590fee7b52d7d962885b4bc42a788fbdaf63f89'
+		);
+
+		const encrypted = Encryption.encrypt(message, privateKey, publicKey, nonce);
+
+		const expected = sodium.from_hex(
+			'78326e930590fee7b52d7d962885b4bc42a788fbdaf63f89d4aadc24d4eb85288c7cbe7151c0f63f61d39fb000003c17fb262b84'
+		);
+
+		expect(encrypted).toEqual(expected);
+	});
+
+	test('encrypt lock state message', () => {
+		const privateKey = sodium.from_hex(
+			'a62d5062d73eb12e7d804e5e07a79083abc5bd0e1a8f9cec46a02c708c0aea60'
+		);
+		const publicKey = sodium.from_hex(
+			'4a15a6a548422383f38a3ecfee5ec830877b9bed5af13eaa2c9bf56ca5221976'
+		);
+		const message = sodium.from_hex('09000200050014a5272b9c6d01f8670209');
+		const nonce = sodium.from_hex(
+			'f57e429667bad667db006a0a93554531e023cf704c1de389'
+		);
+		const expected = sodium.from_hex(
+			'f57e429667bad667db006a0a93554531e023cf704c1de389adb92076cba2ece88cf1228ac2fd45ca50270d900a5befae6c91bf5702bbcf646d'
+		);
+
+		const encrypted = Encryption.encrypt(message, privateKey, publicKey, nonce);
+
+		expect(encrypted).toEqual(expected);
 	});
 });
